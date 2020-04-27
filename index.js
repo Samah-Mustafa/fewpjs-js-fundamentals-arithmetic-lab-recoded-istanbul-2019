6346cee325df02458bf877aa4144c0e695d56b29
samah_mustafa <i.samaaah@gmail.com>
2020-04-28 00:58:00 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
 
 // Write your code here
+
+    
+    const num1 = 2;
+    const num2 = 31;
+    const multiply = num1 * num2;
+
+
+    const random = (Math.random() * 10) + 1;
+
+
+    let mod = 34%5;
+
+    const max = Math.max(1, 2, 3, 4, 20);
+
+
+// functin() {
+    
+//     let num1 = 2;
+//     let num2 = 31;
+    
+//     multiply = num1 * num2;
+
+//     console.log("num1 * num2") ;
+// }
+
+
+// let random => 0;
+// console.log("random");
+
+// let mod = 64 % 5;
+// console.log("mod");
+
+// let max 
